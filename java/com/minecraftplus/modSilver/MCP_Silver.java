6896df413a4301a43040a8193899f95014bb826f
Toby Reed <Toby Reed@idea-PC.Home>
2014-06-27 21:32:04 +0100

    Can you pull it down and have a look?

--- java/com/minecraftplus/modSilver/MCP_Silver.java
+++ java/com/minecraftplus/modSilver/MCP_Silver.java
@@ -1,6 +1,7 @@
 package com.minecraftplus.modSilver;
 
 import com.minecraftplus._base.MCP;
+import com.minecraftplus._base.registry.ItemRegistry;
 import com.minecraftplus._base.registry.Registry;
 
 import cpw.mods.fml.common.Mod;
@@ -11,25 +12,40 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "MCP_" + MCP_Silver.MODBASE, name = "MC+ " + MCP_Silver.MODBASE, version = "1.0.0")
+@Mod(modid = "MCP_Silver" + MCP_Silver.MODBASE, name = "MC+ Silver" + MCP_Silver.MODBASE, version = "1.0.0")
 public class MCP_Silver extends MCP
 {
 	protected static final String MODBASE = "Silver";
 
-	@Instance("MCP_" + MCP_Silver.MODBASE)
+	@Instance("MCP_Silver" + MCP_Silver.MODBASE)
 	public static MCP_Silver INSTANCE;
 
-	@SidedProxy(clientSide = "com.minecraftplus.mod" + MODBASE + ".ClientProxy", serverSide = "com.minecraftplus.mod" + MODBASE + ".CommonProxy")
+	@SidedProxy(clientSide = "com.minecraftplus.modSilver" + MODBASE + ".ClientProxy", serverSide = "com.minecraftplus.modSilver" + MODBASE + ".CommonProxy")
+	
 	public static CommonProxy proxy;
 
 	//TODO: Nothing yet. . .
 
+	public static final Item silverIngot = new ItemSilverIngot().setUnlocalizedName("silver_ingot");
+	public static final Item silverNugget = new ItemSilverNugget().setUnlocalizedName("silver_nugget");
+	public static final Item silverSword = new ItemSilverSword().setUnlocalizedName("silver_sword");
+	
+	public static final Block silverOre = new BlockOreSilver().setBlockName("silver_ore");
+	public static final Block silverBlock = new BlockSilver().setBlockName("block_of_silver");
+			
 	@EventHandler
 	@Override
 	public void preInit(FMLPreInitializationEvent par1Event)
 	{
 		MCP.initMain(par1Event, "1.1");
-
+		
+		ItemRegistry.add(silverIngot);
+		ItemRegistry.add(silverNugget);
+		ItemRegistry.add(silverSword);
+		
+		ItemRegistry.add(silverOre);
+		ItemRegistry.add(silverBlock);
+		
 		proxy.register(Registry.RENDER);
 		proxy.register(Registry.ENTITY);
 		proxy.register(Registry.CUSTOM_ENTITY);
