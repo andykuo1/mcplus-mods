e76068bd504988c0bccbf8aac0781f92b2e73af6
Tobias76 <tobyreed0@gmail.com>
2014-06-27 17:43:03 +0100

    Minecraft Version: 1.7.2 ModAPI Version: 1.1 Description - This is just an expansion to the Ruby Mod
    Changes:
    
    Added;
    
    Sapphires
    Sapphire Ore (Naturally Generating)
    Sapphire Blocks

---
+++ java/modRuby/ItemRuby.java
@@ -0,0 +1,13 @@
+package com.minecraftplus.modRuby;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+import com.minecraftplus._common.item.ItemBase;
+
+public class ItemRuby extends ItemBase
+{
+	public ItemRuby()
+	{
+		super(CreativeTabs.tabMaterials);
+	}
+}
---
+++ java/modRuby/WorldGenBlockSapphire.java
@@ -0,0 +1,29 @@
+package com.minecraftplus.modRuby;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
+import com.minecraftplus._base.worldgen.IWorldGenSurface;
+import com.minecraftplus._base.worldgen.WorldGenBlock;
+
+public class WorldGenBlockSapphire extends WorldGenBlock implements IWorldGenSurface
+{
+	public WorldGenBlockSapphire()
+	{
+		super(MCP_Ruby.SapphireOre);
+	}
+
+	public void generateSurface(World par1World, Random par2Random, int par3, int par4)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			int randPosX = par3 + par2Random.nextInt(16);
+			int randPosY = par2Random.nextInt(70);
+			int randPosZ = par4 + par2Random.nextInt(16);
+
+			new WorldGenMinable(this.block, 2).generate(par1World, par2Random, randPosX, randPosY, randPosZ);
+		}
+	}
+}
