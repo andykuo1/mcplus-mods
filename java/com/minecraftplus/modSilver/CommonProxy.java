9489d1e9d5035d83386f9554ec5c74781ee29767
Toby Reed <Toby Reed@idea-PC.Home>
2014-06-30 17:19:51 +0100

    <1.7.2>[1.1] Added recipes and ish like that!

--- java/com/minecraftplus/modSilver/CommonProxy.java
+++ java/com/minecraftplus/modSilver/CommonProxy.java
@@ -1,5 +1,7 @@
 package com.minecraftplus.modSilver;
 
+import net.minecraft.item.ItemStack;
+
 import com.minecraftplus._base.ICommonProxy;
 import com.minecraftplus._base.registry.Registry;
 
@@ -26,6 +28,16 @@ public class CommonProxy implements ICommonProxy
 	@Override
 	public void register(Registry.RecipeMode par1Registry)
 	{
-
+		par1Registry.addShapedRecipe(new ItemStack(MCP_Silver.silverBlock, 1), new Object[] {
+			"###", "###", "###",
+			Character.valueOf('#'), MCP_Silver.silverIngot});
+		par1Registry.addShapedRecipe(new ItemStack(MCP_Silver.silverIngot, 1), new Object[] {
+			"   ", "###", "###",
+			Character.valueOf('#'), MCP_Silver.silverNugget});
+		par1Registry.addShapelessRecipe(new ItemStack(MCP_Silver.silverIngot, 9), new Object[] {
+			MCP_Silver.silverBlock
+		});
+		par1Registry.addSmelting(MCP_Silver.silverOre, new ItemStack (MCP_Silver.silverIngot), 1);
+		};
 	}
-}
+
