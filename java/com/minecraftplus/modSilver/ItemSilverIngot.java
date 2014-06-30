d720253260ba9d42dfe0d25fc6cf96b53737c548
Toby Reed <Toby Reed@idea-PC.Home>
2014-06-30 16:03:42 +0100

    And here is the Ingot! Wahey!

---
+++ java/com/minecraftplus/modSilver/ItemSilverIngot.java
@@ -0,0 +1,13 @@
+package com.minecraftplus.modSilver;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+import com.minecraftplus._common.item.ItemBase;
+
+public class ItemSilverIngot extends ItemBase
+{
+	public ItemSilverIngot()
+	{
+		super(CreativeTabs.tabMaterials);
+	}
+}
